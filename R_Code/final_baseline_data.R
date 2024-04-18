62026b5d7d207b66411e77fb842d6d20cb0dd1e3
miguelgonzalez9 <m.gonzalezlugo@tilburguniversity.edu>
2024-04-18 13:22:21 +0200

    Added MOE data

--- R_Code/final_baseline_data.R
+++ R_Code/final_baseline_data.R
@@ -73,21 +73,85 @@ colnames(conflict_panel) <- c("year", "codmpio")
 conflict_panel <- conflict_panel %>% left_join_rep(mun_char, by = c("codmpio"))
 
 # Merge
-pol_vio_RD$year <- as.numeric(pol_vio_RD$year) 
 conflict_vars <- colnames(pol_vio_RD)[-c(1,2)]
-conflict_panel <- conflict_panel %>% left_join_rep(pol_vio_RD, by = c("year", "codmpio"))
+pol_vio_RD$year <- as.integer(pol_vio_RD$year)
+conflict_panel <- conflict_panel %>% left_join_rep(pol_vio_RD, by = c("year", "codmpio")) #6808 non-match master 
 
 ## Leaders somos defensores, INDEPAZ ------------------------------ 
 lideres_RD$year <- as.numeric(lideres_RD$year)
-conflict_panel <- conflict_panel %>% left_join_rep(lideres_RD %>% filter(year %in% years), 
+conflict_panel <- conflict_panel %>% left_join_rep(lideres_RD %>% filter(year %in% years), #7872 non-match master
                                                    by = c("year", "codmpio"))
 conflict_vars <- c(conflict_vars, colnames(lideres_RD)[-c(1,2)])
 
 ## SIEVCAC data on belic actions. -------------------------------
+SIEVAC <- read.csv("D:/Documents/GitHub/Thesis/Data/Final_data/SIEVAC_RD.csv")
+conflict_panel <- conflict_panel %>% left_join_rep(SIEVAC, by = c("codmpio", "year")) #11579 non-matched
+conflict_vars <- c(conflict_vars, colnames(SIEVAC)[-c(1,2,3)])
+#SIEVAC[!(SIEVAC$codmpio %in% conflict_panel$codmpio),] #departments data not matched.
 
-conflict_panel <- conflict_panel %>% left_join_rep(SIEVAC, by = c("codmpio", "year"))
-conflict_vars <- c(conflict_vars, colnames(SIEVAC)[-c(1,2)])
-# SIEVAC[!(SIEVAC$codmpio %in% conflict_panel$codmpio),] departments data not matched.
+## MOE 
+path_moe <- "D:/Documents/GitHub/Thesis/Data/MOE/"
+lid_MOE <- read_xlsx(paste0(path_moe, "MOE_lideres.xlsx"))
+lid_MOE <- lid_MOE %>%
+  mutate(partido = partido %>% tolower() %>% stri_trans_general(id = "Latin-ASCII"))
+
+MOE_ideol <- read_xlsx(paste0(path_moe, "MOE_ideologia.xlsx"))
+
+lid_MOE <- lid_MOE %>% 
+  left_join_rep(MOE_ideol %>% distinct(),
+                "partido")
+lid_MOE <- lid_MOE %>% mutate(ideologia = case_when(
+  str_detect(partido, "gsc") ~ 1, 
+  is.na(ideologia) ~ 4, 
+  T ~ ideologia
+))
+
+colnames(lid_MOE) <- c("codmpio", "code_rnec", "dpto", "mun", "date",
+                       "gender", "victim", "sector", "party", "minority",
+                       "type", "rurality", "PDET", "year", "month", "ideol")
+
+# 5-digit codmpio
+lid_MOE$codmpio <- as.character(lid_MOE$codmpio)
+lid_MOE <- lid_MOE %>% 
+  mutate(codmpio = case_when(
+    str_count(codmpio) == 4 ~ paste0("0", codmpio), 
+    T ~ codmpio
+  ))
+# Change category names
+lid_MOE <- lid_MOE %>% mutate(
+  sector = case_when(
+    sector == "Política" ~ "politic", 
+    sector == "Comunal" ~ "comunal", 
+    sector == "Social" ~ "social",
+    T ~ sector
+  ), 
+  type = case_when(
+    type == "Asesinato" ~ "kill", 
+    type == "Amenaza" ~ "threat", 
+    type == "Desaparición" ~ "disappearance", 
+    type == "Atentado" ~ "kill_fail", 
+    type == "Secuestro" ~ "kidnap",
+    type == "VCMP" ~ "gen_vio"
+  )
+)
+
+# Dummies. Change if more variables are needed.
+lid_MOE <- lid_MOE %>% 
+  dummy_columns(select_columns = c("sector", "type", "ideol"),remove_selected_columns = T) %>% 
+  mutate(poli_vio = 1, 
+         pol_kill = type_kill*sector_politic, 
+         left_kill = type_kill*ideol_1,
+         left_threat = type_threat*ideol_1)
+
+
+sum_cols <- c("pol_kill", "left_kill", "left_threat", 
+              "poli_vio")
+lid_MOE <- lid_MOE %>% summarize_data_count(sum_cols, sum_cols) %>% 
+  filter(year != 2024)
+
+conflict_vars <- c(conflict_vars, sum_cols)
+### Add to conflict panel. 5 muns not in panel
+conflict_panel <- conflict_panel %>% left_join_rep(lid_MOE, c("codmpio", "year"))
 
 
 ## Replace NAs for zeros in conflict vars. 
@@ -102,7 +166,7 @@ conflict_panel <- conflict_panel %>% left_join_rep(pop %>% filter(year %in% year
 ### Normalize conflict variables by 1000 persons.
 conflict_panel <- conflict_panel %>% 
   mutate(population = case_when(population == 0 ~ NA, T ~ population)) %>% 
-  mutate(across(conflict_vars, 
+  mutate(across(all_of(conflict_vars), 
                 ~ 100000*.x/population, 
                 .names = "pop_{col}"))
 
@@ -111,6 +175,8 @@ conflict_panel <- conflict_panel %>%
   mutate(across(c(paste0("pop_",conflict_vars),conflict_vars), list(
     lag1 = ~lag(.x, n = 1, order_by = year), 
     lag2 = ~lag(.x, n = 2, order_by = year), 
+    lag3 = ~lag(.x, n = 3, order_by = year), 
+    lag4 = ~lag(.x, n = 4, order_by = year), 
     lead1 = ~lead(.x, n = 1, order_by = year),
     lead2 = ~lead(.x, n = 2, order_by = year),
     lead3 = ~lead(.x, n = 3, order_by = year),
@@ -121,18 +187,23 @@ for (i in conflict_vars) {
   conflict_panel[paste0("lead12_",i)] <- conflict_panel[paste0("lead1_",i)] + conflict_panel[paste0("lead2_",i)]
   conflict_panel[paste0("lead123_",i)] <- conflict_panel[paste0("lead1_",i)] + conflict_panel[paste0("lead2_",i)] + conflict_panel[paste0("lead3_",i)]
   conflict_panel[paste0("lead1234_",i)] <- conflict_panel[paste0("lead1_",i)] + conflict_panel[paste0("lead2_",i)] + conflict_panel[paste0("lead3_",i)] + conflict_panel[paste0("lead4_",i)]
+  
+  conflict_panel[paste0("lag12_",i)] <- conflict_panel[paste0("lag1_",i)] + conflict_panel[paste0("lag2_",i)]
+  conflict_panel[paste0("lag123_",i)] <- conflict_panel[paste0("lag1_",i)] + conflict_panel[paste0("lag2_",i)] + conflict_panel[paste0("lag3_",i)]
 }
+
 # Normalize using population at t. 
 conflict_panel <- conflict_panel %>% 
   mutate(population = case_when(population == 0 ~ NA, T ~ population)) %>% 
-  mutate(across(starts_with("lead12")| starts_with("lead123") | starts_with("lead1234"), 
+  mutate(across(starts_with("lead12")| starts_with("lead123") | starts_with("lead1234")|
+                  starts_with("lag12")| starts_with("lag123"), 
                 ~ 100000*.x/population, 
                 .names = "pop_{col}"))
 
 
 ### Filter municipalities average pop < 300.000
 conflict_panel <- conflict_panel %>% group_by(codmpio) %>% filter(mean(population) <= 300000) %>% ungroup()
-
+#dim(conflict_panel) 12924  
 # Spending data-------------------------------------------
 # Data from 2015-2020. Only needed for RD analysis. 
 # Mun code
@@ -150,13 +221,17 @@ spend_data <- spend_data %>%
 spend_data <- spend_data %>%  mutate(
   # First year of government
   V_payment_diff1 = V_ln_payments_lead1 - V_ln_payments,
+  lag1_V_payment_diff1 = V_ln_payments - V_ln_payments_lag1,
+  lag2_V_payment_diff1 = V_ln_payments_lag1 - V_ln_payments_lag2,
   # Second year of government
   V_payment_diff2 = V_ln_payments_lead2 - V_ln_payments,
   
   # First year of government
   V1_payment_diff1 = V1_ln_payments_lead1 - V1_ln_payments,
+  lag1_V1_payment_diff1 = V1_ln_payments - V1_ln_payments_lag1,
+  lag2_V1_payment_diff1 = V1_ln_payments_lag1 - V1_ln_payments_lag2,
   # Second year of government
-  V1_payment_diff2 = V1_ln_payments_lead2 - V1_ln_payments
+  V1_payment_diff2 = V1_ln_payments_lead2 - V1_ln_payments,
 ) 
 
 
@@ -167,10 +242,8 @@ RD_baseline <- conflict_panel %>% filter(year %in% c(2015,2019)) %>%
   left_join_rep(elect_left, by = c("codmpio", "year"))
 RD_baseline <- RD_baseline %>% left_join_rep(elect_right, by = c("codmpio", "year"))
 RD_baseline <- RD_baseline %>% left_join_rep(spend_data,  by = c("codmpio", "year"))
-# Conflict vars lead1 + lead2
-RD_baseline <- RD_baseline %>% mutate(across())
-conflict_vars
 
 
-#write_dta(RD_baseline, "D:/Documents/GitHub/Thesis/Data/Final_data/RD_data.dta")
 
+write.csv(RD_baseline, "D:/Documents/GitHub/Thesis/Data/Final_data/RD_data.csv")
+saveRDS(list(conflict_vars,spend_vars,control_vars), file="D:/Documents/GitHub/Thesis/Data/Final_data/vnames.RData")
